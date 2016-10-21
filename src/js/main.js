0245ad92f49b5263f1a0065da3be6a0fdcde27c0
Jack Storrs <jackstorrs8@gmail.com>
2016-10-21 14:40:39 -0400

    Search result and playback functions are working properly.

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,23 @@ import $ from 'jquery';
 
 import { CLIENT_ID } from './clientid';
 
-//for audio source, add ClIENT_ID to the end of 'stream_url'
-// `${CLIENT_ID}`
-// <source src="${stream_url}?client_id=${CLIENT_ID}" type="${original_format}">
-
 
 var SC_API = 'https://api.soundcloud.com'
 
+
 function renderHTML (data) {
     var newHTML = `
-        <div class='eachresult'>
+        <div class='eachresult' data-streamurl="${data.stream}?client_id=${CLIENT_ID}" data-format="${data.format}">
           <img src="${data.album_art}" alt="image"/>
           <div id="title">${data.title_artist}</div>
           <div id="genre">${data.genre}</div>
         </div>
-      `
+      `;
     $('.searchresults').append(newHTML)
 }
 
 function resultData(myResults) {
+    $('.searchresults').html("");
     myResults.forEach(function (eachResult) {
         var getData = {
           album_art: eachResult.artwork_url,
@@ -31,22 +29,30 @@ function resultData(myResults) {
         }
         renderHTML(getData);
     });
-    // console.log(myResults);
+
+    $(".eachresult").click(renderPlayer);
 }
-
-
-
 
 function getResults (event) {
   event.preventDefault()
   var searchcontent = $('#searchinput').val();
   var result = getTrackData(searchcontent);
   result.then(resultData);
-  // console.log(result);
 };
 
-
 $('#searchbutton').click(getResults);
+
+function renderPlayer (event) {
+        console.log(event);
+        console.log(event.currentTarget.dataset.streamurl)
+    var audioHTML = `
+    <audio controls="controls" >
+    <source src="${event.currentTarget.dataset.streamurl}" type="audio/${event.currentTarget.dataset.format}">
+    </audio>
+`
+    $(".player").html(audioHTML)
+}
+
 
 $.ajaxSetup({
     data: {
