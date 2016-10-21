d6345b0340ebf2023119cf0ff71d972af4db05f9
Jack Storrs <jackstorrs8@gmail.com>
2016-10-21 12:12:50 -0400

    Created a function which takes the relevant API data and displays it on the HTML for each result.

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,46 @@ import { CLIENT_ID } from './clientid';
 
 //for audio source, add ClIENT_ID to the end of 'stream_url'
 // `${CLIENT_ID}`
-//<source src="${stream_url}?client_id=${CLIENT_ID}" type="${original_format}">
+// <source src="${stream_url}?client_id=${CLIENT_ID}" type="${original_format}">
 
 
 var SC_API = 'https://api.soundcloud.com'
+
+function renderHTML (data) {
+    var newHTML = `
+        <div class='eachresult'>
+          <img src="${data.album_art}" alt="image"/>
+          <div id="title">${data.title_artist}</div>
+          <div id="genre">${data.genre}</div>
+        </div>
+      `
+    $('.searchresults').append(newHTML)
+}
+
+function resultData(myResults) {
+    myResults.forEach(function (eachResult) {
+        var getData = {
+          album_art: eachResult.artwork_url,
+          title_artist: eachResult.title,
+          format: eachResult.original_format,
+          genre: eachResult.genre,
+          stream: eachResult.stream_url
+        }
+        renderHTML(getData);
+    });
+    // console.log(myResults);
+}
+
+
+
 
 function getResults (event) {
   event.preventDefault()
   var searchcontent = $('#searchinput').val();
   var result = getTrackData(searchcontent);
-  console.log(result);
+  result.then(resultData);
+  // console.log(result);
 };
-
 
 
 $('#searchbutton').click(getResults);
